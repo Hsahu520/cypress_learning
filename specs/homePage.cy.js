80477f3c673b67084fa6b506f4018ef3566fe1e3
Hsahu520 <110760236+Hsahu520@users.noreply.github.com>
2024-05-13 13:25:09 +0530

    Updated homePage.cy.js

--- specs/homePage.cy.js
+++ specs/homePage.cy.js
@@ -1,18 +1,31 @@
-import homePage from '../pages/homePage'
+// import homePage from '../pages/homePage'
 
-describe('Home Page', () => {
-  beforeEach(() => {
-    homePage.goto()
-  })
+// describe('Home Page', () => {
+//   beforeEach(() => {
+//     homePage.goto()
+//   })
 
-  it('should find an older post by loading more', () => {
-    const postTitle = 'Cypress.io Review'
-    homePage.findPostByLoadingMore(postTitle)
+//   it('should find an older post by loading more', () => {
+//     const postTitle = 'Cypress.io Review'
+//     homePage.findPostByLoadingMore(postTitle)
 
-    homePage.postTitleExists(postTitle).should('be.visible')
-  })
+//     homePage.postTitleExists(postTitle).should('be.visible')
+//   })
 
-  it('should display 7 posts per page', async () => {
-    homePage.posts().should('have.length', 7)
-  })
-})
+//   it('should display 7 posts per page', async () => {
+//     homePage.posts().should('have.length', 7)
+//   })
+// })
+
+describe('Cypress write/read file exercise', () => {
+  it('write file', () => {
+    cy.writeFile('writeFile/example.txt', 'Cypress is awesome!');
+    cy.writeFile('writeFile/example.txt', ' Make e2e test great again!', {flag:'a+'});
+    //This Cypress command appends ' Make e2e test great again!' to the existing 'example.txt' file, thanks to the {flag:'a+'}
+
+  });
+
+  it('read file', () => {
+    cy.readFile('writeFile/example.txt').should('contain', 'Cypress')
+  });
+});
